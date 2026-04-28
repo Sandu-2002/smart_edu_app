c6043f607e64a36358ac067d08eb0f0f37847847
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2026-04-28 15:51:04 +0530

    create parent floating navigation bar structure

---
+++ lib/features/parent/widgets/parent_bottom_bar.dart
@@ -0,0 +1,10 @@
+class ParentFloatingBar extends StatelessWidget {
+  const ParentFloatingBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Bottom Nav'),
+    );
+  }
+}
