233f303c2b328f4a00ef62a77190f3d1d06f3f9f
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2026-04-28 09:17:02 +0530

    create parent dashboard screen structure

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -8,6 +8,7 @@ import '../../features/admin/student_success_screen.dart';
 import '../../features/auth/forgot_password_screen.dart';
 import '../../features/auth/login_screen.dart';
 import '../../features/parent/parent_shell.dart';
+import '../../features/parent/dashboard_screen.dart';
 import '../../features/splash/splash_screen.dart';
 import '../../features/student/guide_books_screen.dart';
 import '../../features/student/short_notes_screen.dart';
---
+++ lib/features/dashboard_screen.dart
@@ -0,0 +1,12 @@
+class ParentDashboardScreen extends StatelessWidget {
+  const ParentDashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Parent Dashboard'),
+      ),
+    );
+  }
+}
