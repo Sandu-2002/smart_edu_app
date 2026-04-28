c6ae4cf6369ee0963e6f8437e17ca75ef5965ee3
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2026-04-28 13:40:26 +0530

    add top header with title and subtitle, add floating action button for new message, add mock chat data list

--- lib/features/parent/chat_screen.dart
+++ lib/features/parent/chat_screen.dart
@@ -1,11 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../../core/constants/app_colors.dart';
+
 class ParentChatScreen extends StatelessWidget {
   const ParentChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final chats = [
+      {
+        'name': 'Mr. Nuwan Silva',
+        'message': 'Homework uploaded',
+      },
+      {
+        'name': 'Mrs. Dilki Perera',
+        'message': 'Please sign the report',
+      },
+    ];
+
     return Scaffold(
+      backgroundColor: AppColors.background,
+
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppColors.primaryBlue,
+        onPressed: () {},
+        child: const Icon(
+          Icons.add_comment_outlined,
+          color: Colors.white,
+        ),
+      ),
+
       body: Center(
-        child: Text('Parent Chat'),
+        child: Text('Parent Chat (${chats.length})'),
       ),
     );
   }
