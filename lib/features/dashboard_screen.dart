07f6884c90ee99e281b9cbe75ff714e9be79eef4
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2026-04-28 09:27:06 +0530

    add top header with user info

--- lib/features/dashboard_screen.dart
+++ lib/features/dashboard_screen.dart
@@ -1,11 +1,67 @@
+import 'package:flutter/material.dart';
+
+import '../../core/widgets/top_blue_header.dart';
+import '../../core/widgets/circular_avatar.dart';
+
 class ParentDashboardScreen extends StatelessWidget {
   const ParentDashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Parent Dashboard'),
+      body: Column(
+        children: [
+          TopBlueHeader(
+            height: 240,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: const [
+                Row(
+                  children: [
+                    CircularAvatar(radius: 28),
+                    SizedBox(width: 12),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Hello, Sewwandi',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          SizedBox(height: 4),
+                          Text(
+                            'Parent Portal',
+                            style: TextStyle(
+                              color: Colors.white70,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Icon(
+                      Icons.notifications_none_rounded,
+                      color: Colors.white,
+                      size: 28,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+
+          ///temporary body
+          const Expanded(
+            child: Center(
+              child: Text('Parent Dashboard'),
+            ),
+          ),
+        ],
       ),
     );
   }
