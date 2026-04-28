ae358b112fac958354c8216f08991f657fd31f11
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2026-04-28 15:53:15 +0530

    add navigation state handling props, add material navigation bar widget

--- lib/features/parent/widgets/parent_bottom_bar.dart
+++ lib/features/parent/widgets/parent_bottom_bar.dart
@@ -1,10 +1,31 @@
+import 'package:flutter/material.dart';
+
 class ParentFloatingBar extends StatelessWidget {
   const ParentFloatingBar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Bottom Nav'),
+      child: NavigationBar(
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.dashboard_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.auto_graph_outlined),
+            label: 'Results',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.chat_bubble_outline),
+            label: 'Chat',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person_outline),
+            label: 'Profile',
+          ),
+        ],
+      ),
     );
   }
 }
